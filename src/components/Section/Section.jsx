1edb6b661017d155c8a8498e3044bdd8a3f6bb48
Yuliia Nivina <y.nivina@gmail.com>
2023-05-06 22:05:59 +0300

    Add HW-2

---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types';
+
+const Section = ({ title, children }) => (
+  <section>
+    <h1>{title}</h1>
+    {children}
+  </section>
+);
+
+export default Section;
+
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+}
